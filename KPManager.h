65b5ecd0df5d385698e0400e2eaae0f1c2abccf1
Andreas Henriksson <andreaskhenriksson@gmail.com>
2020-04-02 16:47:30 +0200

    Cleanup

--- KPManager.h
+++ KPManager.h
@@ -1,8 +1,8 @@
 @interface KPManager : NSObject
 @property (nonatomic, assign, readonly) BOOL trialEnded;
 @property (nonatomic, retain, readonly) NSString *immortalPartyingBundleID;
-- (void)killImmortalPID:(int)pid;
 - (void)setTrialEnded;
 - (void)setup;
+- (void)killImmortalPID:(int)pid;
 - (NSDictionary *)getAllProcessStates;
 @end
